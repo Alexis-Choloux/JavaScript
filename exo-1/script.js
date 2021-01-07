486252a3c598d769eb0d4c8b947fb0c553050d81
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-07 16:49:51 +0100

    3 - all done

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,10 @@
-// you can write js here
-console.log('hello from file');
+let kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
+
+let celsius = kelvin - 273;
+// dans la variable celsius, j'utilise la variable kelvin pour ma soustraction
+
+let fahrenheit = celsius * (9/5) + 32;
+// j'utilise la variable celsius pour mon calcul, dans la variable déclarée 'fahrenheit'
+
+console.log(Math.floor(fahrenheit));
+// j'utilise la fonction interne math.floor pour arrondir le résultat de fahreneitz
--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,69 @@
 // you can write js here
 console.log('exo-3');
+
+function userChoice (userInput) {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'damme' || userInput === 'stalone' || userInput === 'trump' || userInput === 'norris') {
+        return userInput;
+    } else {
+        console.log('Please, choose damme, stalone, or trump!');
+    }
+}
+
+function getComputerChoice () {
+    let number = Math.floor(Math.random() * 3);
+    let computerChoice;
+
+    switch (number) {
+        case 0:
+            computerChoice = 'damme';
+            break;
+        case 1:
+            computerChoice = 'stalone';
+            break;
+        case 2:
+            computerChoice = 'trump';
+            break;
+    }
+    return computerChoice;
+}  
+
+function determineWinner (userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return 'Tied!';
+    }
+    else if (userChoice === 'stalone') {
+        if (computerChoice === 'damme') {
+            return 'Won! Stalone is stronger, of course!';
+        } else {
+            return 'Lost! Trump expulse Stalone to Russia!';
+        }
+    }
+    else if (userChoice === 'damme') {
+        if (computerChoice === 'trump') {
+            return 'Won! Damme say something... Trump\'s head explode!';
+        } else {
+            return 'Lost! Stalone become Rocky, and punch Damme in the face!';
+        }
+    }
+    else if (userChoice === 'trump') {
+        if (computerChoice === 'stalone') {
+            return 'Won! Trump build a wall between him and Stalone!';
+        } else {
+            return 'Lost! Damme say something... Trump\'s head explode!!';
+        }
+    }
+    else if (userChoice === 'norris') {
+            return 'Won! Norris killed everyone!';
+    }
+}
+
+function playGame () {
+    let uChoice = userChoice(prompt("Stalone, Damme, Trump ?"));
+    let computerChoice = getComputerChoice();
+    console.log('You chose: ' + uChoice);
+    console.log('The computer chose: ' + computerChoice);
+    console.log(determineWinner(uChoice, computerChoice));
+}
+
+playGame();
