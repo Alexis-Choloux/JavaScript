6954bd8b7455cff927e7d4cf45ddce35430a8ea3
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-08 10:44:10 +0100

    5 - done / 6 - in progress

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,20 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = 'I like to spit with my back!';
+input = input.toLowerCase();
+
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+let resultArray = [];
+
+for (let letter of input) {
+    console.log(letter);
+    if (vowels.indexOf(letter) !== -1) {
+        resultArray.push(letter);
+    }
+}
+
+console.log(resultArray.join(''));
+// s'il y a des quotes dans join(), le resultat s'afficher en string (donc sans virgules)
--- exo-6/exo6.js
+++ exo-6/exo6.js
@@ -11,7 +11,79 @@ var joeInfo = {
 };
 
 // afficher le nombre de voitures de Joe
+console.log(joeInfo.cars.length);
 
 // changer le nombre de salle de bains de Joe : il n'en possède au'une.
+joeInfo.bathrooms = 1;
 
 // Joe vient d'acquérir un garage changer la structure pour le refléter.
+joeInfo.garage = true;
+
+// ===========================================================================================
+
+// functions
+let addPlayer = (first, last, age) => team._players.push(
+    {
+        firstName: first,
+        lastName: last,
+        age: age
+    }
+);
+
+let addGame = (oppo, tPoints, oPoints) => team._games.push(
+    {
+        opponent: oppo,
+        teamPoints: tPoints,
+        opponentPoint: oPoints
+    }
+);
+
+
+// js
+let team = {
+    _players: [
+        {
+            firstName: 'Johan',
+            lastName: 'Himal',
+            age: 29
+        }, 
+        {
+            firstName: 'Florine',
+            lastName: 'Taigueur',
+            age: 27
+        },
+        {
+            firstName: 'Hugo',
+            lastName: 'Blet',
+            age: 32
+        },
+        {
+            firstName: 'Alexis',
+            lastName: 'Lophone',
+            age: 28
+        }
+    ],
+    _games: [
+        {
+            opponent: 'Beauxbaton',
+            teamPoints: 25,
+            opponentPoint: 50
+        },
+        {
+            opponent: 'Poudlard',
+            teamPoints: 68,
+            opponentPoint: 3
+        },
+        {
+            opponent: 'Durmstrong',
+            teamPoints: 29,
+            opponentPoint: 17
+        }
+    ]
+}
+
+addPlayer('Emma', 'Nifique', 22);
+
+addGame('Magne', 43, 32);
+
+console.log(team);
