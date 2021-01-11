bf808001f7408792958bd74de9f727fb553a013a
Alexis Choloux <alexis.choloux.pro@gmail.com>
2021-01-11 10:43:26 +0100

    ex 7 - almost done

--- exo-7/datas.js
+++ exo-7/datas.js
@@ -1,57 +1,86 @@
-var jsonDatas=[
+var jsonDatas = [
 	{
-		"name" :"Fiat Punto",
-		"type" : "car",
-		"description" : "Je suis une voiture",
-		"price" : 10000,
-		"quantity" : 2
-	},{
-		"name" :"Porsche 911",
-		"type" : "car",
-		"description" : "Je suis une belle voiture",
-		"price" : 80000,
-		"quantity" : 0
-	},{
-		"name" :"Villa sur la plage",
-		"type" : "house",
-		"description" : "Quelle belle vue",
-		"price" : 870000,
-		"quantity" : 1
-	},{
-		"name" :"Peugeot 205",
-		"type" : "car",
-		"description" : "Je suis une autre voiture",
-		"price" : 2000,
-		"quantity" : 2
-	},{
-		"name" :"Maison à la campagne",
-		"type" : "house",
-		"description" : "Vive le calme",
-		"price" : 170000,
-		"quantity" : 3
-	},{
-		"name" :"Monopoly",
-		"type" : "game",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 300
-	},{
-		"name" :"Mario Bros",
-		"type" : "videoGame",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
-	},{
-		"name" :"Place VIP concert Metallica",
-		"type" : "show",
-		"description" : "",
-		"price" : 800,
-		"quantity" : 10
-	},{
-		"name" :"Entrée au parc Astérix",
-		"type" : "show",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
+		"type": "car",
+		"items": [
+			{
+				"name": "Fiat Punto",
+				"description": "Je suis une voiture",
+				"price": 10000,
+				"quantity": 2
+			},
+			{
+				"name": "Porsche 911",
+				"description": "Je suis une belle voiture",
+				"price": 80000,
+				"quantity": 0,
+			},
+			{
+				"name": "Peugeot 205",
+				"description": "Je suis une autre voiture",
+				"price": 2000,
+				"quantity": 2
+			},
+		]
+	},
+	{
+		"type": "house",
+		"items": [
+			{
+				"name": "Villa sur la plage",
+				"description": "Quelle belle vue",
+				"price": 870000,
+				"quantity": 1
+			},
+			{
+				"name": "Maison à la campagne",
+				"type": "house",
+				"description": "Vive le calme",
+				"price": 170000,
+				"quantity": 3
+			},
+		]
+	},
+	{
+		"type": "game",
+		"items": [
+			{
+				"name": "Monopoly",
+				"type": "game",
+				"description": "",
+				"price": 30,
+				"quantity": 300
+			}
+		]
+	},
+	{
+		"type": "videoGame",
+		"items": [
+			{
+				"name": "Mario Bros",
+				"type": "videoGame",
+				"description": "",
+				"price": 30,
+				"quantity": 200
+			}
+		]
+	},
+	{
+		"type": "show",
+		"items": [
+			{
+				"name": "Place VIP concert Metallica",
+				"type": "show",
+				"description": "",
+				"price": 800,
+				"quantity": 10
+			},
+			{
+				"name": "Entrée au parc Astérix",
+				"type": "show",
+				"description": "",
+				"price": 30,
+				"quantity": 200
+			}
+		]
 	}
 ]
--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,117 @@
-console.log("exo-7");
+jsonDatas.forEach(element => console.log(element));
+
+let traduction = {
+    "car": "voiture",
+    "house": "logement",
+    "game": "jeu",
+    "videoGame": "jeuVidéo",
+    "show": "événement"
+}
+
+jsonDatas.forEach(element => element.translatedType = traduction[element.type]);
 
 console.log(jsonDatas);
+
+// Input & button & checkbox
+let input = document.createElement('input');
+document.body.append(input);
+
+let text = document.querySelector('input');
+
+
+const button = document.createElement('button');
+button.textContent = 'Develop!';
+document.body.append(button);
+
+const alphaBtn = document.createElement('button');
+alphaBtn.textContent = 'Tri alphabétique';
+
+const invAlphaBtn = document.createElement('button');
+invAlphaBtn.textContent = 'Tri alphabétique inversé';
+
+const ascPriceBtn = document.createElement('button');
+ascPriceBtn.textContent = ('Prix croissant');
+
+const descPriceBtn = document.createElement('button');
+descPriceBtn.textContent = 'Prix décroissant';
+
+
+const checkbox = document.createElement('input');
+checkbox.setAttribute('type', 'checkbox');
+document.body.append(checkbox);
+
+
+
+button.onclick = () => {
+    for (let i = 0; i < jsonDatas.length; i++) {
+        if (text.value === jsonDatas[i].type) {
+
+            let articles = [];
+
+            if (checkbox.checked) {
+
+                for (let j = 0; j < jsonDatas[i].items.length; j++) {
+
+                    if (jsonDatas[i].items[j].quantity > 0) {
+                        articles.push(jsonDatas[i].items[j]);
+                        console.log(articles);
+
+                        document.body.append(alphaBtn);
+                        document.body.append(invAlphaBtn);
+                        document.body.append(ascPriceBtn);
+                        document.body.append(descPriceBtn);
+
+                        alphaBtn.onclick = () => {
+                            articles.sort((a, b) => (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0);
+                            console.log(articles);
+                        }
+
+                        invAlphaBtn.onclick = () => {
+                            articles.sort((a, b) => (a.name > b.name) ? -1 : (a.name < b.name) ? 1 : 0);
+                            console.log(articles);
+                        }
+
+                        ascPriceBtn.onclick = () => {
+                            articles.sort((a, b) => (a.price < b.price) ? -1 : (a.price > b.price) ? 1 : 0);
+                            console.log(articles);
+                        }
+
+                        descPriceBtn.onclick = () => {
+                            articles.sort((a, b) => (a.price > b.price) ? -1 : (a.price < b.price) ? 1 : 0);
+                            console.log(articles);
+                        }
+                    }
+                }
+            }
+            else {
+                articles.push(jsonDatas[i]);
+                console.log(articles);
+
+                document.body.append(alphaBtn);
+                document.body.append(invAlphaBtn);
+                document.body.append(ascPriceBtn);
+                document.body.append(descPriceBtn);
+
+                alphaBtn.onclick = () => {
+                    articles.sort((a, b) => (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0);
+                    console.log(articles);
+                }
+
+                invAlphaBtn.onclick = () => {
+                    articles.sort((a, b) => (a.name > b.name) ? -1 : (a.name < b.name) ? 1 : 0);
+                    console.log(articles);
+                }
+
+                ascPriceBtn.onclick = () => {
+                    articles.sort((a, b) => (a.price < b.price) ? -1 : (a.price > b.price) ? 1 : 0);
+                    console.log(articles);
+                }
+
+                descPriceBtn.onclick = () => {
+                    articles.sort((a, b) => (a.price > b.price) ? -1 : (a.price < b.price) ? 1 : 0);
+                    console.log(articles);
+                }
+            }
+        }
+    }
+}
